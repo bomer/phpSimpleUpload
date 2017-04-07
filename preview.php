ec5431c657438b6799f593ab096388471cfbce9d
James O'Toole <jimmeyotoole@gmail.com>
2017-04-07 13:05:40 +1000

    Api and preview page

---
+++ preview.php
@@ -0,0 +1,16 @@
+<?php 
+
+$dir    = 'uploads/';
+$files  = scandir($dir);
+$out=[];
+
+foreach ($files as $key => $value) {
+	if (in_array($value, [".","..",".DS_Store"])){
+		unset($files[$key]);
+	}
+	else 
+		echo "<a href='/uploads/$value'> <img src='/uploads/$value'> </a>" ;
+}
+// echo json_encode($out);
+
+?>
